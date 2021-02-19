cd8e2a865e3a62dd773d160bb075317d9d727021
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-02-18 23:33:42 -0800

    finished writing method to check weather ternary

--- Ternary_Challenge/Program.cs
+++ Ternary_Challenge/Program.cs
@@ -6,7 +6,37 @@ namespace Ternary_Challenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //takes a temperature value as input and checks if the input is an integer or not
+
+            int temp;
+            bool check;
+            Console.WriteLine("What is the temperature in Celcius?");
+            string tempCheck = Console.ReadLine();
+
+            check = int.TryParse(tempCheck, out temp); 
+
+            if (check)
+            {
+                CheckWeather(temp);
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid temperature!");
+            }
+            
+            
+           
+        }
+
+        public static void CheckWeather(int temp)
+        {
+            string message = temp <= 15 ?
+            "it is too cold here" :
+            (temp >= 16 && temp <= 28) ?
+            "it is ok" :
+            "it is hot here";
+
+            Console.WriteLine(message);
         }
     }
 }
